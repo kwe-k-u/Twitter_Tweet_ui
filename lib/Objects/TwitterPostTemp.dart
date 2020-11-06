61e9c9ba5d5f538862826ea829142f9452d3a003
Kweku <kwekuappiah11@gmail.com>
2020-11-06 20:00:05 +0000

    Added documentation for the objects

--- lib/Objects/TwitterPostTemp.dart
+++ lib/Objects/TwitterPostTemp.dart
@@ -1,6 +1,3 @@
-import 'dart:ffi';
-
-import 'package:flutter/material.dart';
 import 'package:twitter_tweets_ui/Objects/TwitterUserTemp.dart';
 
 class TwitterPostTemp{
@@ -34,6 +31,7 @@ class TwitterPostTemp{
 
   //                  SETTERS
 
+  ///Creates and returns a [TwitterPostTemp] object from the json data [map]
   TwitterPostTemp createFromMap(Map<dynamic, dynamic> map){
     setDatePosted(map["created_at"]);
     setTweetId(map["id"]);
@@ -53,33 +51,38 @@ class TwitterPostTemp{
     return this;
   }
 
+  ///Sets the tweet ID of [TwitterPostTemp] to [id]
   void setTweetId(dynamic id){
     this._idString = id.toString();
   }
 
 
-
+///Sets the date of posting of [TwitterPostTemp] to [date]
   void setDatePosted(dynamic date){
     if (date.runtimeType == String)
         this._createdAt = convertTwitterDate(date);
     else if (date.runtimeType == DateTime)
       this._createdAt = date;
   }
-  
+
+
+  ///Sets the number of characters of in [_tweetText] to the range [range]
   void setTweetCharacterRange(List<dynamic> range){
     this._tweetCharacterRange = range;
   }
   
 
-
+///Sets the account with which [TwitterPostTemp] was posted to [user]
   void setUser(TwitterUserTemp user){
     this._user = user;
   }
 
+  ///Sets the text of [TwitterPostTemp] to [text]
   void setTweetText(String text){
     this._tweetText = text; //todo add checker to keep length 140 characters
   }
 
+  /// Sets the
   void setFavouriteStatus(bool status){
     this._isFavourite = status;
   }
@@ -88,45 +91,57 @@ class TwitterPostTemp{
     this._isRetweeted = status;
   }
 
+  ///Sets the possibly sensitive status of [TwitterPostTemp] to [status]
   void setIsSensitiveStatus(bool status){
     this._isPossiblySensitive = status;
   }
 
+  ///Sets the truncated status of [TwitterPostTemp] to [status]
   void setTruncatedStatus(bool status){
     this._isTruncated = status;
   }
 
+  ///Sets the url to the tweet to [url]
   void setTweetURL(String url){
     this._tweetUrl = url;
   }
 
+  ///Sets the number of favourites of [TwitterPostTemp] to [count]
   void setFavouriteCount(int count){
     this._favouriteCount = count;
   }
 
-
+///Sets the number of retweets of [TwitterPostTemp] to [count]
   void setRetweetCount(int count){
     this._retweetCount = count;
   }
 
+
+  ///Sets the number of replies to [TwitterPostTemp] to [count]
   void setReplyCount(int count){
     this._replyCount = count;
   }
 
+
+  ///Sets the number of quoted tweets of [TwitterPostTemp] to [count]
   void setQuoteCount (int count){
     this._quoteCount = count;
   }
 
+
+  ///Sets the language of [TwitterPostTemp] to [lang]
   void setLanguage(String lang){
     this._language = lang;
   }
 
+
+  ///Sets the filter level of [TwitterPostTemp] to [level]
   void setFilterLevel(String level){
     this._filterLevel = level;
   }
 
   
-
+//todo add support for other languages besides english
 
 
 
@@ -136,64 +151,83 @@ class TwitterPostTemp{
 
   //                    GETTERS
 
+  ///Returns the date [TwitterPostTemp] was posted on Twitter
   DateTime getDateCreated()
   => this._createdAt;
 
 
-
+///Returns the user who posted [TwitterPostTemp] as a [TwitterUserTemp] object
   TwitterUserTemp getUser(){
     return this._user;
   }
 
+
+  ///Returns the id of the tweet [TwitterPostTemp] as a string
   String getTweetId()
   => this._idString;
 
 
+  ///Returns the text of the tweet
   String getTweetText(){
     return this._tweetText;
   }
 
 
+  ///Returns a boolean to show the truncation status of [TwitterPostTemp]
   bool isTruncated()
   => this._isTruncated;
 
+  ///Returns a boolean to show the if the post has been marked as favourite
   bool isFavourite(){
     return this._isFavourite;
   }
 
+  ///Returns the url of the tweet [TwitterPostTemp]
   String getTweetURL()
   => this._tweetUrl;
 
+  ///Returns a boolean to show if the tweet [TwitterPostTemp] has been retweeted
   bool isRetweeted()
   => this._isRetweeted;
 
+
+  ///Returns a boolean to show if the tweet [TwitterPostTemp] contains sensitive material
   bool isSensitive()
   => this._isPossiblySensitive;
 
+
+  ///Returns the number of likes the tweet has
   int getFavouriteCount()
   => this._favouriteCount;
 
+  ///Returns the number of retweets the tweet has
   int getRetweetCount()
   => this._retweetCount;
 
+  ///Returns the number of comments or replies the tweet has
   int getReplyCount()
   => this._replyCount;
 
+  ///Returns the number of quoted tweets the tweet has
   int getQuoteCount()
   => this._quoteCount;
 
 
+  ///Returns teh filter level of the tweet as a string
   String getFilterLevel()
       => this._filterLevel;
 
+  ///Returns the language of the tweet as shorthand String
   String getLanguage()
   => this._language;
   
-  
+
+  ///Returns a list showing the number of characters in the tweet
   List<dynamic> getTweetCharacterRange()
   => this._tweetCharacterRange;
 
 
+  ///Returns a json object containing all the attributes of [TwitterPostTemp]
   Map<String,dynamic> getMap(){
     Map<String,dynamic> map;
 
@@ -202,6 +236,7 @@ class TwitterPostTemp{
     return map;
   }
 
+  ///Obtains a date format as a string and converts it into a dart DateTime object
   DateTime convertTwitterDate(String d){
     DateTime newDate;
     List<String> _months = ["Jan", "Feb",	"Mar", 	"Apr",	"May", 	"June", 	"July",
--- lib/Objects/TwitterUserTemp.dart
+++ lib/Objects/TwitterUserTemp.dart
@@ -18,6 +18,7 @@ class TwitterUserTemp{
   //entities
 
 
+  ///Creates a [TwitterUserTemp] from a json object [map]
   TwitterUserTemp createFromMap(Map<String,dynamic> map){
 
     setUserHandle(map["screen_name"]);
@@ -30,31 +31,37 @@ class TwitterUserTemp{
   }
 
 
+  ///Sets the account name of [TwitterUserTemp] to [name]
   void setUserName(String name){
     this._userAccountName = name;
   }
 
 
+  ///Sets the user handle of [TwitterUserTemp] to [handle]
   void setUserHandle(String handle){
     this._userHandle = handle;
   }
 
 
+  ///Sets the link to the Twitter profile as [url]
   void setProfileUrl(String url){
     this.__profileUrl = url;
   }
 
 
+  ///Sets the url of the Twitter profile's image to [url]
   void setProfileImageUrl(String url){
     this._profileImageUrl = url;
   }
 
 
+  ///Sets the account id of [TwitterUserTemp] to [id]
   void setUserId(String id){
     this._userId = id;
   }
 
 
+  ///Sets the date of creation of the twitter account to [date]
   void setCreatedDate(dynamic date){
     if (date.runtimeType == String){
       this._createAt = convertTwitterDate(date);
@@ -64,36 +71,43 @@ class TwitterUserTemp{
   }
 
 
+  ///Sets the profile description of the Twitter account to [description]
   void setDescription(String description){
     this._description = description;
   }
 
 
+  ///Sets the location of the twitter account as the string [location]
   void setLocation(String location){
     this._location = location;
   }
 
 
+  ///Sets the protected status of the twitter account to [status]
   void setProtectedStatus(bool status){
     this._isProtected = status;
   }
 
 
+  ///Sets the verified status of the twitter account to [status]
   void setVerifiedStatus(bool status){
     this._isVerified = status;
   }
 
 
+  ///Sets the number of followers of [TwitterUserTemp] to [count]
   void setFollowersCount(int count){
     this._followerCount = count;
   }
 
 
+  ///Sets the number of accounts followed by [TwitterUserTemp] to [count]
   void setFollowingCount(int count){
     this._followingCount = count;
   }
 
 
+  ///Sets the number of tweets of [TwitterUserTemp] to [count]
   void setTweetCount(int count){
     this._tweetCount = count;
   }
@@ -109,54 +123,72 @@ class TwitterUserTemp{
 
 
 
+  ///Returns the account handle of [TwitterUserTemp] as a string
   String getUserHandle()
   => "@"+this._userHandle ?? "";
 
+  ///Returns the account name of [TwitterUserTemp] as a string
   String getAccountName()
   => this._userAccountName ?? "";
 
+  ///Returns the url to the twitter profile of [TwitterUserTemp]
   String getProfileUrl()
   => this.__profileUrl ?? "";
 
 
+  ///Returns the url to the profile image of the twitter account
+  String getProfileImageUrl()
+  => this._profileImageUrl ?? "";
+
+
+
+  ///Returns the Twitter account id of [TwitterUserTemp] as a string
   String getUserId()
   => this._userId ?? "";
 
+  ///Returns the date of creation of the Twitter account of [TwitterUserTemp]
   DateTime getDateCreated()
   => this._createAt;
 
+  ///Returns the description of the twitter account of [TwitterUserTemp]
   String getDescription()
   => this._description ?? "";
 
 
+  ///Returns the location of the twitter account of [TwitterUserTemp] as a string
   String getLocation()
   => this._location ?? "";
 
 
-  String getProfileImageUrl()
-  => this._profileImageUrl ?? "";
 
+  ///Returns a boolean to show if the twitter account is protected
   bool isProtected()
   => this._isProtected ?? false;
 
+  ///Returns a boolean to show if the twitter account is verified
   bool isVerified()
   => this._isVerified ?? false;
 
+  ///Returns the number of followers of the twitter account
   int getFollowerCount()
   => this._followerCount;
 
+  ///Returns the number of accounts that the twitter account follows
   int getFollowingCount()
   => this._followingCount;
+
 
   int getListedCount()
   => this._listedCount;
 
+  ///Returns the number of tweets by [TwitterUserTemp]
   int getTweetCount()
   => this._tweetCount;
 
 
 
 
+  ///Obtains a date format as a string and converts it into a dart DateTime object
   DateTime convertTwitterDate(String d){
     DateTime newDate;
     List<String> _months = ["Jan", "Feb",	"Mar", 	"Apr",	"May", 	"June", 	"July",
